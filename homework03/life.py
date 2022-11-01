16cd9e9a4e77616ef6104c5659af17789b7131ae
Pavel <hurs_pasha@mail.ru>
2022-11-01 19:30:12 +0300

    first try

--- homework03/life.py
+++ homework03/life.py
@@ -15,7 +15,7 @@ class GameOfLife:
         self,
         size: tp.Tuple[int, int],
         randomize: bool = True,
-        max_generations: tp.Optional[float] = float("inf"),
+        max_generations: float = float("inf"),
     ) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
@@ -29,46 +29,89 @@ class GameOfLife:
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        grid: Grid = []
+        for i in range(self.rows):
+            grid.append([])
+            for j in range(self.cols):
+                grid[i].append(random.randint(0, int(randomize)))
+
+        return grid
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+        row, col = cell
+        answer = []
+
+        positions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        for i in positions:
+            newrow, newcol = row + i[0], col + i[1]
+            if newrow < 0 or newrow >= self.rows or newcol < 0 or newcol >= self.cols:
+                continue
+            answer.append(self.curr_generation[newrow][newcol])
+        return answer
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        newgrid: Grid = []
+
+        for i in range(self.rows):
+            newgrid.append([])
+            for j in range(self.cols):
+                neighbours = self.get_neighbours((i, j))
+                count = 0
+                for n in neighbours:
+                    if n:
+                        count += 1
+
+                if count == 2 and self.curr_generation[i][j]:
+                    newgrid[i].append(1)
+                    continue
+
+                if count == 3:
+                    newgrid[i].append(1)
+                    continue
+
+                newgrid[i].append(0)
+
+        return newgrid
 
     def step(self) -> None:
-        """
-        Выполнить один шаг игры.
-        """
-        pass
+        self.prev_generation = [[j for j in i] for i in self.curr_generation]
+        self.curr_generation = self.get_next_generation()
+        self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
-        """
-        Не превысило ли текущее число поколений максимально допустимое.
-        """
-        pass
+        return self.generations >= self.max_generations
 
     @property
     def is_changing(self) -> bool:
-        """
-        Изменилось ли состояние клеток с предыдущего шага.
-        """
-        pass
+        return self.curr_generation != self.prev_generation
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
-        """
-        Прочитать состояние клеток из указанного файла.
-        """
-        pass
+        f = open(filename, "r")
+
+        grid: Grid = []
+        for i in f.readlines():
+            grid.append([])
+            for j in i:
+                grid[-1].append(int(j))
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        a = GameOfLife(size=(rows, cols), randomize=False)
+        a.curr_generation = grid
+
+        f.close()
+
+        return a
 
     def save(self, filename: pathlib.Path) -> None:
-        """
-        Сохранить текущее состояние клеток в указанный файл.
-        """
-        pass
+        f = open(filename, "w")
+
+        for i in self.curr_generation:
+            for j in i:
+                f.write(str(j))
+            f.write("\n")
+
+        f.close()
--- homework03/life_proto.py
+++ homework03/life_proto.py
@@ -17,6 +17,10 @@ class GameOfLife:
         self.height = height
         self.cell_size = cell_size
 
+        self.rows = self.height // self.cell_size
+        self.cols = self.width // self.cell_size
+        self.grid = self.create_grid(randomize=True)
+
         # Устанавливаем размер окна
         self.screen_size = width, height
         # Создание нового окна
@@ -29,91 +33,47 @@ class GameOfLife:
         # Скорость протекания игры
         self.speed = speed
 
-    def draw_lines(self) -> None:
-        """ Отрисовать сетку """
-        for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
-        for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
-
-    def run(self) -> None:
-        """ Запустить игру """
-        pygame.init()
-        clock = pygame.time.Clock()
-        pygame.display.set_caption("Game of Life")
-        self.screen.fill(pygame.Color("white"))
-
-        # Создание списка клеток
-        # PUT YOUR CODE HERE
-
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    running = False
-            self.draw_lines()
-
-            # Отрисовка списка клеток
-            # Выполнение одного шага игры (обновление состояния ячеек)
-            # PUT YOUR CODE HERE
-
-            pygame.display.flip()
-            clock.tick(self.speed)
-        pygame.quit()
-
     def create_grid(self, randomize: bool = False) -> Grid:
-        """
-        Создание списка клеток.
+        grid: Grid = []
+        for i in range(self.rows):
+            grid.append([])
+            for j in range(self.cols):
+                grid[i].append(random.randint(0, int(randomize)))
 
-        Клетка считается живой, если ее значение равно 1, в противном случае клетка
-        считается мертвой, то есть, ее значение равно 0.
-
-        Parameters
-        ----------
-        randomize : bool
-            Если значение истина, то создается матрица, где каждая клетка может
-            быть равновероятно живой или мертвой, иначе все клетки создаются мертвыми.
-
-        Returns
-        ----------
-        out : Grid
-            Матрица клеток размером `cell_height` х `cell_width`.
-        """
-        pass
-
-    def draw_grid(self) -> None:
-        """
-        Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
-        """
-        pass
+        return grid
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        """
-        Вернуть список соседних клеток для клетки `cell`.
+        row, col = cell
+        answer = []
 
-        Соседними считаются клетки по горизонтали, вертикали и диагоналям,
-        то есть, во всех направлениях.
-
-        Parameters
-        ----------
-        cell : Cell
-            Клетка, для которой необходимо получить список соседей. Клетка
-            представлена кортежем, содержащим ее координаты на игровом поле.
-
-        Returns
-        ----------
-        out : Cells
-            Список соседних клеток.
-        """
-        pass
+        positions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        for i in positions:
+            newrow, newcol = row + i[0], col + i[1]
+            if newrow < 0 or newrow >= self.rows or newcol < 0 or newcol >= self.cols:
+                continue
+            answer.append(self.grid[newrow][newcol])
+        return answer
 
     def get_next_generation(self) -> Grid:
-        """
-        Получить следующее поколение клеток.
+        newgrid: Grid = []
 
-        Returns
-        ----------
-        out : Grid
-            Новое поколение клеток.
-        """
-        pass
+        for i in range(self.rows):
+            newgrid.append([])
+            for j in range(self.cols):
+                neighbours = self.get_neighbours((i, j))
+                count = 0
+                for n in neighbours:
+                    if n:
+                        count += 1
+
+                if count == 2 and self.grid[i][j]:
+                    newgrid[i].append(1)
+                    continue
+
+                if count == 3:
+                    newgrid[i].append(1)
+                    continue
+
+                newgrid[i].append(0)
+
+        return newgrid
